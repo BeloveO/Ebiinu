7f87416136374bdb3c76e2585cdbb5a39fb350a4
Belove <beloveolusola@gmail.com>
2023-11-10 05:06:31 +0100

    Guest checkout

--- ebi/models.py
+++ ebi/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=1)
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
@@ -35,6 +35,15 @@ class Order(models.Model):
     
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
     
     @property
     def get_cart_total(self):
--- ebi/urls.py
+++ ebi/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.ebi, name='ebi'),
     path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
 
 ]
